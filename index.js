2a9f1928955dcc59f75c323cd2952d69046f5bf3
Elaina <elainaseki@gmail.com>
2018-12-07 20:05:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+var dwarves=["Doc", "Dopey", "Bashful", "Grumpy"];
 function dwarfRollCall(dwarves) {
+  var arr=[];
+  for(var i=0; i<dwarves.length; i++){
+    var string = `${i+1}. ${dwarves[i]} `;
+    arr.push(string);
+  }
+  return arr.join('');
 }
 
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 function summonCaptainPlanet(planeteerCalls){
+   var arr=[];
+  for(var i=0; i<planeteerCalls.length; i++){
+   var string = `${planeteerCalls[i]}!`;
+    arr.push(string.toUpperCase());
+  }
+  return arr;
 }
 
 function longPlaneteerCalls(words) {
+  if(words.length>3){
+    return true;
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  var cheese = ['cheddar', 'gouda', 'camembert'];
+  for(var i=0;i<cheese.length;i++){
+    for(var j=0; j<foods.length; j++){
+      if(cheese[i]===foods[j]){
+      return cheese[i];
+    }
+   }
+  }
+   return `no cheese!`;
 }
